1cb36e1fc89933d2dc034a2d700a524487bad370
RAMF-Meza <ramon.meza.ramf@outlook.com>
2023-11-07 23:35:20 -0600

    Object detection solution

--- object-detection/packages/solution/integration_activity.py
+++ object-detection/packages/solution/integration_activity.py
@@ -38,13 +38,10 @@ def filter_by_classes(pred_class: int) -> bool:
     # TODO: Change this to only return True for duckies!
     # In other words, returning False means that this prediction is ignored.
 
-    out = False
-    if (out == 0){
-        out = True
-    } else {
-        out = False
-    }
-    return out
+    if pred_class == 0:
+        return True
+    else:
+        return False
 
 
 def filter_by_scores(score: float) -> bool:
@@ -55,7 +52,11 @@ def filter_by_scores(score: float) -> bool:
     # Right now, this returns True for every object's confidence
     # TODO: Change this to filter the scores, or not at all
     # (returning True for all of them might be the right thing to do!)
-    return True
+
+    if score > .80:
+        return True
+    else:
+        return False
 
 
 def filter_by_bboxes(bbox: Tuple[int, int, int, int]) -> bool:
@@ -65,4 +66,17 @@ def filter_by_bboxes(bbox: Tuple[int, int, int, int]) -> bool:
                 This means the shape of bbox is (leftmost x pixel, topmost y, rightmost x, bottommost y)
     """
     # TODO: Like in the other cases, return False if the bbox should not be considered.
-    return True
+
+    b = bbox[2] - bbox[0]
+    h = bbox[1] - bbox[3]
+    a = b * h
+    a_tot = 640*480
+
+    # If the box is less than x %, ignore
+    if a <= 0.20*a_tot:
+        return False
+    # If the box is in the left most third, ignore
+    elif (bbox[2] < 210) or (bbox[0] > 435):
+        return False
+    else:
+        return True
